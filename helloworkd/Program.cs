7b5df0e0996f67e9e54d341247505867b794f1e3
paulovmdutra <121131923+dutrapaulovm@users.noreply.github.com>
2024-09-18 14:30:00 -0300

    First Commit

---
+++ helloworkd/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> nomes = [];
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Digite seu nome: ");
+            var nome = Console.ReadLine();
+        }
+
+        IEnumerable<string> query = nomes.Where<string>(item => item.ToLower() == "paulo".ToLower());
+
+        foreach (var item in query)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+
+}
+
+
+
